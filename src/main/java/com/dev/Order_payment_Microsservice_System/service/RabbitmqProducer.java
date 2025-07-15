ae6ce749693c4db7b19b6436ab8d8c8599e13400
Thales <thales@triliton.com.br>
2025-07-15 16:46:52 -0300

    feat: create RabbitmqProducer

---
+++ src/main/java/com/dev/Order_payment_Microsservice_System/service/RabbitmqProducer.java
@@ -0,0 +1,17 @@
+package com.dev.Order_payment_Microsservice_System.service;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitmqProducer {
+    @Autowired
+    private  AmqpTemplate amqpTemplate;
+
+    public void send(String exchange, String routingKey, Object playload){
+        amqpTemplate.convertAndSend(exchange,routingKey,playload);
+        System.out.println("Success send message!"+ exchange + routingKey+ playload);
+    }
+
+}
