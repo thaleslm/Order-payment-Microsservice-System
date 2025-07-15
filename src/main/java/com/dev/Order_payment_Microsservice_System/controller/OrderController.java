606d68a1430b5d66fa8de788601b70ee5cecfe6b
Thales <thales@triliton.com.br>
2025-07-15 16:47:10 -0300

    feat: create OrderController

---
+++ src/main/java/com/dev/Order_payment_Microsservice_System/controller/OrderController.java
@@ -0,0 +1,10 @@
+package com.dev.Order_payment_Microsservice_System.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("api/order")
+public class OrderController {
+}
