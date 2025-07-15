5376b3e2ab1dd92a8279271ca67dde5ed60cc05b
Thales <thales@triliton.com.br>
2025-07-15 16:48:36 -0300

    ...

---
+++ src/main/java/com/dev/Order_payment_Microsservice_System/OrderPaymentMicrosserviceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Order_payment_Microsservice_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentMicrosserviceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentMicrosserviceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/Order_payment_Microsservice_System/OrderPaymentMicrosserviceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Order_payment_Microsservice_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentMicrosserviceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
