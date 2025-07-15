0b88defd06e12430cf78c9e610b94dd75787b84e
Thales <thales@triliton.com.br>
2025-07-15 10:44:27 -0300

    feat: create Order entity

---
+++ src/main/java/com/dev/Order_payment_Microsservice_System/domain/order/Order.java
@@ -0,0 +1,24 @@
+package com.dev.Order_payment_Microsservice_System.domain.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="Orders")
+@Table(name="Orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private StatusType status;
+}
---
+++ src/main/java/com/dev/Order_payment_Microsservice_System/domain/order/StatusType.java
@@ -0,0 +1,5 @@
+package com.dev.Order_payment_Microsservice_System.domain.order;
+
+public enum StatusType {
+    RECEIVED, PROCESSED
+}
