ee5d9fe5d12cc7d97e1ea55b043a2c02ac309ff8
Thales <thales@triliton.com.br>
2025-07-17 15:26:03 -0300

    feat: add validation in Controller

--- src/main/java/com/dev/Order_payment_Microsservice_System/controller/OrderController.java
+++ src/main/java/com/dev/Order_payment_Microsservice_System/controller/OrderController.java
@@ -1,22 +1,21 @@
 package com.dev.Order_payment_Microsservice_System.controller;
 
-import com.dev.Order_payment_Microsservice_System.dto.OrderDTO;
+import com.dev.Order_payment_Microsservice_System.dtos.OrderDTO;
 import com.dev.Order_payment_Microsservice_System.service.OrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
 @RequestMapping("/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
     @PostMapping
-    private void enqueOrder(OrderDTO order){
-
-
+    private ResponseEntity<String> enqueOrder(@RequestBody @Valid OrderDTO order){
+        this.orderService.enqueueOrder(order);
+        return ResponseEntity.ok("Order created successfully");
     }
 }
