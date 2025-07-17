4c43a46d6b3982031a66261a29608479432c11bb
Thales <thales@triliton.com.br>
2025-07-17 11:22:24 -0300

    feat: create OrderController

--- src/main/java/com/dev/Order_payment_Microsservice_System/controller/OrderController.java
+++ src/main/java/com/dev/Order_payment_Microsservice_System/controller/OrderController.java
@@ -1,10 +1,22 @@
 package com.dev.Order_payment_Microsservice_System.controller;
 
+import com.dev.Order_payment_Microsservice_System.dto.OrderDTO;
+import com.dev.Order_payment_Microsservice_System.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("api/order")
+@RequestMapping("/order")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    private void enqueOrder(OrderDTO order){
+
+
+    }
 }
