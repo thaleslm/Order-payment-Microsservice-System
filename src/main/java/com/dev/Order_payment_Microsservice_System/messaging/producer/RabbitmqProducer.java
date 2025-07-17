c04a0e729ffd541444941b51b6b9ca775a0b718d
Thales <thales@triliton.com.br>
2025-07-17 11:24:02 -0300

    feat: change structure file

--- src/main/java/com/dev/Order_payment_Microsservice_System/messaging/producer/RabbitmqProducer.java
+++ src/main/java/com/dev/Order_payment_Microsservice_System/messaging/producer/RabbitmqProducer.java
@@ -1,4 +1,4 @@
-package com.dev.Order_payment_Microsservice_System.service;
+package com.dev.Order_payment_Microsservice_System.messaging.producer;
 
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/dev/Order_payment_Microsservice_System/service/OrderProducerService.java
+++ src/main/java/com/dev/Order_payment_Microsservice_System/service/OrderProducerService.java
@@ -1,21 +1,23 @@
 package com.dev.Order_payment_Microsservice_System.service;
 
 import com.dev.Order_payment_Microsservice_System.domain.order.Order;
+import com.dev.Order_payment_Microsservice_System.messaging.producer.RabbitmqProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderProducer {
+public class OrderProducerService {
 
     @Value("${queue.exchange.order}")
-    public String QUEUE_EXCHANGE ;
+    private String QUEUE_EXCHANGE ;
     @Value("${queue.routing.key.order}")
-    public String ORDER_KEY_QUEUE ;
+    private String ORDER_KEY_QUEUE ;
     @Autowired
-    private  RabbitmqProducer rabbitmqProducer;
+    private RabbitmqProducer rabbitmqProducer;
 
     public void SendOrderToQueue(Order order){
         this.rabbitmqProducer.send(QUEUE_EXCHANGE,ORDER_KEY_QUEUE,order);
+
     }
 }
